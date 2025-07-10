a2d61938c11b81253002661a5997fdc56b6bc2df
Zoey <dazam1@naver.com>
2025-07-11 00:10:50 +0900

    config 브라우저 설정 변경

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
@@ -14,7 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   timeout: 30_000,
   globalTimeout: 10 * 60 * 1000,
-  testDir: './tests',
+  testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -24,36 +24,44 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://practicesoftwaretesting.com',
+    baseURL: "https://practicesoftwaretesting.com",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on',
+    trace: "on",
     actionTimeout: 0,
     ignoreHTTPSErrors: true,
-    video: 'retain-on-failure',
-    screenshot: 'only-on-failure',
-    headless: true
+    video: "retain-on-failure",
+    screenshot: "only-on-failure",
+    headless: true,
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "setup", // 이 설정의 이름 (리포트/로그에서 구분용)
+      testMatch: /.*\.setup\.ts/, // `.setup.ts`로 끝나는 테스트 파일만 실행 대상
     },
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "chromium",
+      dependencies: ["setup"],
+      use: { ...devices["Desktop Chrome"], permissions: ["clipboard-read"] },
     },
 
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "firefox",
+      dependencies: ["setup"],
+      use: { ...devices["Desktop Firefox"], permissions: ["clipboard-read"] },
+    },
+
+    {
+      name: "webkit",
+      dependencies: ["setup"],
+      use: { ...devices["Desktop Safari"], permissions: ["clipboard-read"] },
     },
 
     /* Test against mobile viewports. */
